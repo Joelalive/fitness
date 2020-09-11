a949fe590b3bd297ae371733aab1055a8d494464
joelalive <joelimalive1994@gmail.com>
2020-09-11 17:44:13 +0530

    angular flexlayout, EP -28

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
+import { TrainingComponent } from './training/training.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'training',
+    component: TrainingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
